3fa7b9eb6e8d920f6aec112f23f7a1d7b646913e
barry cumbie <barrycumbie@gmail.com>
2022-04-14 09:07:58 -0500

    initial code

---
+++ scripts/fetchJohnDoe.js
@@ -0,0 +1,21 @@
+function jget() {
+
+    console.log("in jget");
+    // (A) FETCH REQUEST
+    fetch("data/john-doe.json")
+
+    // (B) RETURN SERVER RESPONSE AS TEXT
+    .then((result) => {
+            console.log(result);
+            // if (result.status != 200) { throw new Error("Bad Server Response"); }
+            return result.json();
+        })
+        .then((response) => {
+            console.log(response);
+        })
+
+    // (C) HANDLE ERROR
+    .catch((error) => { console.log(error); });
+}
+
+$('#btnLoadData').click(jget());
